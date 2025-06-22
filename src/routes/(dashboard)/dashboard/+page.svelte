bdc5167fbdb2911e95d82dd45446b37550d69406
Benedict Mondini <benmondini@netcode.dev>
2025-06-22 16:19:53 +0200

    Changed adapter, update dependencies

--- src/routes/(dashboard)/dashboard/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="mx-auto max-w-xl p-8">
-	<h1 class="mb-4 text-2xl font-bold">Welcome to your Dashboard</h1>
-	<p class="mb-4">You are now logged in.</p>
-	<button
-		class="rounded bg-[#1b9aaa] p-2 text-white">
-		Logout
-	</button>
-</div>
